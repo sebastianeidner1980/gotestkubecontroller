3172ce292cf9e5072f563de912c6701322fcfb28
Sebastian Eidner <seidner@redhat.com>
2023-03-07 01:05:04 +0100

    adjusted config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,18 +19,21 @@ type Configuration struct {
 func NewConfiguration(appName string) *Configuration {
 	var C Configuration
 	cfgFile := strings.Split(ConfigFile, ".")
-	viper.SetConfigName(cfgFile[0])          // name of config file (without extension)
-	viper.SetConfigType(cfgFile[1])          // REQUIRED if the config file does not have the extension in the name
+	viper.SetConfigName(cfgFile[0]) // name of config file (without extension)
+	viper.SetConfigType(cfgFile[1]) // REQUIRED if the config file does not have the extension in the name
+
 	viper.AddConfigPath("$HOME/." + appName) // call multiple times to add many search paths
 	viper.AddConfigPath("./" + cfgFile[0])   // optionally look for config in the working directory
-	err := viper.ReadInConfig()              // Find and read the config file
-	if err != nil {                          // Handle errors reading the config file
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 	err = viper.Unmarshal(&C)
+
 	if err != nil {
 		panic(fmt.Errorf("fatal error could completly parse config file: %w", err))
 	}
 
-	return &C, nil
+	return &C
 }
